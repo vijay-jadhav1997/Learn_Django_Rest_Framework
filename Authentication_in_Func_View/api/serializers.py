dd3101dd8d07d7147c1b5d4e28df1586d0f4e24d
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-06-06 10:28:49 +0530

    learn 'Function Based Authentication' in DRF

---
+++ Authentication_in_Func_View/api/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from .models import Student
+
+#* Validators
+def start_with_r(value):
+  if value[0].lower() != 'r':
+    raise serializers.ValidationError("Name should start with R.")
+
+class StudentSerializer1(serializers.Serializer):
+  name = serializers.CharField( max_length=70, validators=[start_with_r])
+  roll = serializers.IntegerField( )
+  city = serializers.CharField( max_length=70)
+
+  def create(self, validate_data):
+    return Student.objects.create(**validate_data)
+  
+  def validate_roll(self, value):
+    # print(value)
+    if value >= 200:
+      raise serializers.ValidationError("Admission has been closed !")
+    return value
+
+  def validate(self, data):
+    # print(data)
+    if data.get('name').lower().startswith('p') and data.get('roll') > 50:
+      raise serializers.ValidationError(f"When Student name start with 'p', the roll number must be less than 50 💯")
+    return data
+
+
+class StudentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Student
+    fields = "__all__"
---
+++ Authentication_in_Func_View/api/views.py
@@ -0,0 +1,241 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+import io
+
+from .models import Student
+from .serializers import StudentSerializer
+
+# Create your views here.
+def home(request, stud_id=None):
+  if stud_id is not None :
+    stud = Student.objects.get(pk=stud_id)
+    serializer = StudentSerializer(stud)
+  else:
+    stud = Student.objects.all()
+    serializer = StudentSerializer(stud, many=True)
+
+  # print('\n\n 🌼', stud, stud.name)
+  # print('\n\n ✨', serializer)
+  # print('\n\n 🙏🏻', serializer.data)
+  json_data = JSONRenderer().render(serializer.data)
+  # print('\n\n 💯', json_data)
+
+  return HttpResponse(
+    json_data, content_type='application/json'
+  )
+
+def students_info(request):
+  students = Student.objects.all()
+  serializer = StudentSerializer(students, many=True)
+
+  # data_ = JSONRenderer().render(serializer.data)
+  # return HttpResponse(data_)
+
+  return JsonResponse(serializer.data, safe=False)
+  
+
+def student_data(request, stud_id):
+  students = Student.objects.all()
+  serializer = StudentSerializer(students, many=True)
+
+  data_ = JSONRenderer().render(serializer.data)
+  # return HttpResponse(data_)
+
+  return JsonResponse(serializer.data)
+  
+@csrf_exempt
+def student_create(request):
+  if request.method == 'GET':
+    data = request.body
+    stream = io.BytesIO(data)
+    python_data = JSONParser().parse(stream)
+    id = python_data.get('id', None)
+    if id is not None:
+      students = Student.objects.get(id=id)
+      serializer = StudentSerializer(students)
+    else:  
+      students = Student.objects.all()
+      serializer = StudentSerializer(students, many=True)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
+
+  
+  if request.method == 'POST':
+    json_data = request.body
+    stream = io.BytesIO(json_data)
+    python_data = JSONParser().parse(stream)
+    serializer = StudentSerializer(data=python_data)
+
+    if serializer.is_valid():
+      serializer.save()
+      response = {'message': f'New student name {python_data.get('name')}, admission successfully completed!🎉✨'}
+      json_response = JSONRenderer().render(response)
+      return HttpResponse(json_response, content_type='application/json')
+    
+    json_response = JSONRenderer().render(serializer.errors)
+    return HttpResponse(json_response, content_type='application/json')
+    
+
+
+@api_view(['GET', 'POST'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def jay_hari(request):
+  if request.method == 'GET':
+    return Response({'message': '!! ...Jay Jay Ram krushna Hari... !!'})
+
+  if request.method == 'POST':
+    # stream = io.BytesIO(request.body)
+    # data = JSONParser().parse(stream)
+    print('\n\n 💥🌼 ', request.headers, "\n\n 💯 ", request.body)
+    return Response({**request.data})
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def om_shree(request):
+  if request.method == 'GET':
+    id = request.data.get('id')
+    if id is not None:
+      student = Student.objects.get(id=id)
+      serializer = StudentSerializer(student)
+    else:
+      student = Student.objects.all()
+      serializer = StudentSerializer(student, many=True)
+    return Response(serializer.data)
+      # return Response({'message': '!! ...Jay Jay Ram krushna Hari... !!'})
+
+  if request.method == 'POST':
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message':'Admission of the new student is successful.. 🌼🎉'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors)
+  
+
+  if request.method == "PUT":
+    id = request.data.get('id')
+    student = Student.objects.get(id=id)
+    serializer = StudentSerializer(student, data=request.data)
+    # serializer = StudentSerializer(student, data=request.data, partial=True)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message': f"The data of student id '{id}' is updated successfully...🎉💯"})
+    return Response(serializer.errors)
+
+
+  if request.method == "DELETE":
+    id = request.data.get('id')
+    student = Student.objects.get(id=id)
+    student.delete()
+    return Response({'message': f"The student id '{id}' is deleted successfully... ❌💯"})
+
+
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def namo_narayan(request, stud_id=None):
+  if request.method == 'GET':
+    if stud_id is not None:
+      student = Student.objects.get(pk=stud_id)
+      serializer = StudentSerializer(student)
+    else:
+      student = Student.objects.all()
+      serializer = StudentSerializer(student, many=True)
+    return Response(serializer.data)
+      # return Response({'message': '!! ...Jay Jay Ram krushna Hari... !!'})
+
+  if request.method == 'POST':
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message':'Admission of the new student is successful.. 🌼🎉'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+
+  if request.method == "PUT":
+    student = Student.objects.get(pk=stud_id)
+    serializer = StudentSerializer(student, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message': f"The data of student id '{stud_id}' is updated successfully...🎉💯"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+  if request.method == "PATCH":
+    student = Student.objects.get(pk=stud_id)
+    serializer = StudentSerializer(student, data=request.data, partial=True)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message': f"The data of student id '{stud_id}' is updated successfully...🎉💯"}, status=status.HTTP_206_PARTIAL_CONTENT)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+  if request.method == "DELETE":
+    student = Student.objects.get(pk=stud_id)
+    student.delete()
+    return Response({'message': f"The student id '{stud_id}' is deleted successfully... ❌💯"})
+
+
+
+
+
+#! Class Based API View:
+class StudentAPI(APIView):
+  """
+    List all students, create a new student, update or delete instance of a student.
+  """
+  def get(self, request, stud_id=None, format=None):
+    if stud_id is not None:
+      student = Student.objects.get(pk=stud_id)
+      serializer = StudentSerializer(student)
+    else:
+      student = Student.objects.all()
+      serializer = StudentSerializer(student, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+  
+  def post(self, request, stud_id=None, format=None):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message':'New student admission is completed successfully ... 🌼🤾🏻‍♂️'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+  def put(self, request, stud_id=None, format=None):
+    student = Student.objects.get(pk=stud_id)
+    serializer = StudentSerializer(student, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message': f"The data of student id '{stud_id}' is updated successfully...🎉💯"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+  def patch(self, request, stud_id=None, format=None):
+    student = Student.objects.get(pk=stud_id)
+    serializer = StudentSerializer(student, data=request.data, partial=True)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'message': f"The data of student id '{stud_id}' is updated successfully...🎉💯"}, status=status.HTTP_206_PARTIAL_CONTENT)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+  def delete(self, request, stud_id, format=None):
+    student = Student.objects.get(pk=stud_id)
+    student.delete()
+    return Response({'message': f"The student id '{stud_id}' is deleted successfully... ❌💯"})
+
