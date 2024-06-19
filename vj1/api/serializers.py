cb93e83dd678c0312b370b5acebef537331bebd8
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-06-19 19:46:41 +0530

    update vj1

--- vj1/api/serializers.py
+++ vj1/api/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 
-from .models import Student
+# from .serializers import Student
 
 
 
-class StudentSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Student
-    fields = "__all__"
+# 
+class StudentSerializer(serializers.Serializer):
+  name = serializers.CharField(max_length=100)
+  roll = serializers.IntegerField()
+  city = serializers.CharField(max_length=100)
+
+  pass
+
+
+
+
+# class StudentSerializer(serializers.ModelSerializer):
+#   class Meta:
+#     model = Student
+#     fields = "__all__"
